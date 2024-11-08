0918a89c82b33fa7924a24143ba201fd2781b65b
borisey <slamsnv@gmail.com>
2024-11-08 20:55:19 +0300

    Init

--- src/main/java/com/borisey/personal_finance/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.borisey.personal_finance.repo;
-
-import com.borisey.personal_finance.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUUID(String UUID);
-}
