1bb43396117e61fa8bf88318866dc22cb01b27d3
borisey <slamsnv@gmail.com>
2024-11-09 11:12:07 +0300

    Category model

--- src/main/java/com/borisey/personal_finance/models/Category.java
+++ src/main/java/com/borisey/personal_finance/models/Category.java
@@ -1,9 +1,12 @@
 package com.borisey.personal_finance.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Category {
@@ -11,5 +14,16 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private Long user_id, parent_id;
+
+    // Тип категории (доходы или расходы)
+    private Byte type_id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime created;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updated;
 
 }
