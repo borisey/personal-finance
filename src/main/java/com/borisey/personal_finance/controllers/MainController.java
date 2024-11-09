37ead51f3b4d6798e7fc0ab673df356972d19d78
borisey <slamsnv@gmail.com>
2024-11-09 19:12:06 +0300

    date

--- src/main/java/com/borisey/personal_finance/controllers/MainController.java
+++ src/main/java/com/borisey/personal_finance/controllers/MainController.java
@@ -1,20 +1,17 @@
 package com.borisey.personal_finance.controllers;
 
 import com.borisey.personal_finance.models.Account;
+import com.borisey.personal_finance.models.Balance;
 import com.borisey.personal_finance.models.Category;
-import com.borisey.personal_finance.models.User;
 import com.borisey.personal_finance.repo.AccountRepository;
+import com.borisey.personal_finance.repo.BalanceRepository;
 import com.borisey.personal_finance.repo.CategoryRepository;
 import com.borisey.personal_finance.repo.UserRepository;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Objects;
 
 @Controller
 public class MainController {
@@ -25,6 +22,8 @@ public class MainController {
     private UserRepository userRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private BalanceRepository balanceRepository;
 
     @GetMapping("/")
     public String accountAdd(Model model) {
@@ -51,6 +50,11 @@ public class MainController {
         // Общая сумма на всех счетах
         Iterable<Account> allUserAmount = accountRepository.findSumByUserId(1L);
         model.addAttribute("allUserAmount", allUserAmount);
+
+        // Передаю в вид все транзакции пользователя
+        // todo передавать ID реального пользователя
+        Iterable<Balance> allUserTransactions = balanceRepository.findByUserId(1L, Sort.by(Sort.Direction.DESC, "date"));
+        model.addAttribute("allUserTransactions", allUserTransactions);
 
         return "home";
     }
