78923768d3eba42b607edf18be8edd4feec6ee24
borisey <slamsnv@gmail.com>
2024-11-16 13:18:44 +0300

    fix

--- src/main/java/com/borisey/personal_finance/controllers/AccountController.java
+++ src/main/java/com/borisey/personal_finance/controllers/AccountController.java
@@ -1,15 +1,15 @@
 package com.borisey.personal_finance.controllers;
 
-import com.borisey.personal_finance.models.Account;
-import com.borisey.personal_finance.models.User;
+import com.borisey.personal_finance.models.*;
 import com.borisey.personal_finance.repo.AccountRepository;
 import com.borisey.personal_finance.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class AccountController {
@@ -39,6 +39,57 @@ public class AccountController {
         account.setUpdated(currentDateTime);
 
         accountRepository.save(account);
+
+        return "redirect:/my";
+    }
+
+    // Редактирование дохода
+    @GetMapping("/account/{id}/edit")
+    public String accountEdit(@PathVariable(value = "id") Long id, Model model) {
+
+        // Получаю ID текущего пользователя
+        User currentUser = userService.getCurrentUser();
+        Long userId = currentUser.getId();
+        String username = currentUser.getUsername();
+
+        // todo искать также по пользователю
+        Account transaction = accountRepository.findById(id).orElseThrow();
+
+        // todo Если статью добавил не этот пользователь (запретить редактирование)
+
+        Optional<Account> link = accountRepository.findById(id);
+        ArrayList<Account> res = new ArrayList<>();
+        link.ifPresent(res::add);
+        model.addAttribute("account", res);
+
+        return "account-edit";
+    }
+
+    @PostMapping("/account/{id}/edit")
+    public String accountEdit(@PathVariable(value = "id") Long id,
+                             @RequestParam
+                             Float amount,
+                             String title
+    ) {
+        // todo проверять пользователя
+        // Сохраняю счет
+        Account account = accountRepository.findById(id).orElseThrow();
+        account.setAmount(amount);
+        account.setTitle(title);
+        accountRepository.save(account);
+
+        return "redirect:/my";
+    }
+
+    @GetMapping("/account/{id}/delete")
+    public String linkLinkRemove(@PathVariable(value = "id") long id, Model model) {
+
+        // todo искать также по ID пользователя, чтобы запретить удалить чужие записи
+        Account account = accountRepository.findById(id).orElseThrow();
+
+        // todo доработать Если транзакцию добавил не этот пользователь
+
+        accountRepository.delete(account);
 
         return "redirect:/my";
     }
