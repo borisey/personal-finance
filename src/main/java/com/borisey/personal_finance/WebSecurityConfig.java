2c55707479e9654d667476795787c61fc0f3aacc
borisey <slamsnv@gmail.com>
2024-11-21 21:28:21 +0300

    robots.txt

--- src/main/java/com/borisey/personal_finance/WebSecurityConfig.java
+++ src/main/java/com/borisey/personal_finance/WebSecurityConfig.java
@@ -23,7 +23,14 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/register", "/robots.txt", "/landing/**", "/account/**")
+                        .requestMatchers(
+                                "/",
+                                "/register",
+                                "/robots.txt",
+                                "/sitemal.xml",
+                                "/landing/**",
+                                "/account/**"
+                        )
                         .permitAll()
                         .anyRequest()
                         .authenticated()
