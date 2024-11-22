da89f1bd8f9bc2c305dcd6f8588841e488d1120a
borisey <slamsnv@gmail.com>
2024-11-22 20:11:36 +0300

    analytics

---
+++ src/main/java/com/borisey/personal_finance/controllers/AnalyticsController.java
@@ -0,0 +1,41 @@
+package com.borisey.personal_finance.controllers;
+
+import java.util.Map;
+import java.util.TreeMap;
+import com.borisey.personal_finance.models.Category;
+import com.borisey.personal_finance.models.User;
+import com.borisey.personal_finance.repo.CategoryRepository;
+import com.borisey.personal_finance.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AnalyticsController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/analytics")
+    public String getPieChart(Model model) {
+
+        // Получаю ID текущего пользователя
+        User currentUser = userService.getCurrentUser();
+        Long userId = currentUser.getId();
+
+        // Получаю все категории доходов
+        Iterable<Category> allUserIncomeCategories = categoryRepository.findByUserIdAndTypeId(userId, (byte) 1, Sort.by(Sort.Direction.DESC, "id"));
+
+        Map<String, Double> chartIncomeData = new TreeMap<>();
+        for (Category category : allUserIncomeCategories) {
+            chartIncomeData.put(category.getTitle(), category.getAllamount());
+        }
+
+        model.addAttribute("chartIncomeData", chartIncomeData);
+
+        return "analytics";
+    }
+}
