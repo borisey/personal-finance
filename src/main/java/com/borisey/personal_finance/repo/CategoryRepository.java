51ac7db3236f0ff6eddff9bf1e24b4bf1534ba06
borisey <slamsnv@gmail.com>
2024-11-25 19:09:33 +0300

    refactoring

--- src/main/java/com/borisey/personal_finance/repo/CategoryRepository.java
+++ src/main/java/com/borisey/personal_finance/repo/CategoryRepository.java
@@ -9,14 +9,14 @@ import java.time.LocalDateTime;
 public interface CategoryRepository extends CrudRepository<Category, Long> {
     Iterable<Category> findAll(Sort colName);
 
-    @Query(value = "SELECT category.id, category.budget, category.title, category.created, category.parent_id, category.type_id, category.updated, category.user_id, sum(t.amount) as allamount "
+    @Query(value = "SELECT category.id, category.budget, category.title, category.created, category.parent_id, category.type_id, category.updated, category.user_id, sum(balance.amount) as allamount "
             + " FROM category category "
-            + " LEFT JOIN balance t ON (t.category_id=category.id) "
+            + " LEFT JOIN balance balance ON (balance.category_id=category.id) "
             + " WHERE category.user_id=?1 "
             + " AND category.type_id=?2 "
-            + " AND (t.type_id=category.type_id OR t.type_id IS NULL) "
-            + " AND t.date>=?3 "
-            + " AND t.date<=?4 "
+            + " AND (balance.type_id=category.type_id OR balance.type_id IS NULL) "
+            + " AND balance.date>=?3 "
+            + " AND balance.date<=?4 "
             + " GROUP BY category.id "
             , nativeQuery = true)
     Iterable<Category> findByUserIdAndTypeIdAmount(Long userId, Byte typeId, LocalDateTime dateTimeFrom, LocalDateTime dateTimeTo, Sort colName);
